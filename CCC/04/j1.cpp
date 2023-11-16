07dfca8b6b02be011dfc30fb909528a662b28b03
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-11-16 17:24:25 -0500

    Solved J1 04.

---
+++ CCC/04/j1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int tiles;
+    std::cin >> tiles;
+
+    int ans = (int) std::sqrt(tiles);
+
+    std::cout << "The largest square has side length " << ans << ".";
+}
