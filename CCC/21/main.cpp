eee44b2a58ab32838cedf8feb2be793d39186620
Aryan Vasudevan <vasudevan.aryan@gmail.com>
2023-12-08 16:53:52 -0500

    Solved J1 21.

---
+++ CCC/21/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int b;
+    std::cin >> b;
+
+    int p = 5 * b - 400;
+    std::cout << p << "\n";
+    
+    if (p < 100) {
+        std::cout << 1;
+    } else if (p > 100) {
+        std::cout << -1;
+    } else {
+        std::cout << 0;
+    }
+
+    return 0;
+}
